69ad3eebc6ef1dff184d795ad07076b9393509a4
Daniël Illouz <daniel.illouz@me.com>
2018-12-17 19:59:12 +0100

    Fetch a URL body provided via command line arg

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	flag.Parse()
+	urls := flag.Args()
+
+	if len(urls) < 1 {
+		log.Fatal("provide an URL, for example https://news.ycombinator.com/news")
+	}
+
+	url := urls[0]
+	fmt.Printf("crawling %s", url)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("body", string(body))
 }
