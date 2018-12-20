a80cf08043aba2e0bc08af6449297ad562f10e94
Daniël Illouz <daniel.illouz@me.com>
2018-12-20 18:33:02 +0100

    Make depth configurable as a command line flag

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 
 	"github.com/danillouz/spidey/pkg/cache"
 	"github.com/danillouz/spidey/pkg/crawler"
@@ -12,15 +12,16 @@ import (
 func main() {
 	urip := flag.String("uri", "", "The URI of the page to crawl. For example 'https://news.ycombinator.com/news'.")
 	filterp := flag.String("filter", "", "Only these links will be crawled. For example '/news'.")
+	depthp := flag.Int("depth", 1, "How many levels of child links will be crawled. For example '6'")
 	flag.Parse()
 
-	fmt.Println("uri", *urip)
-	fmt.Println("filter", *filterp)
+	if *urip == "" {
+		log.Fatal("A URI must be provided in order to crawl a webpage.")
+	}
 
 	f := fetcher.New()
 	c := cache.New()
 	cr := crawler.New(f, c)
 
-	depth := 3
-	cr.Crawl(*urip, *filterp, depth)
+	cr.Crawl(*urip, *filterp, *depthp)
 }
